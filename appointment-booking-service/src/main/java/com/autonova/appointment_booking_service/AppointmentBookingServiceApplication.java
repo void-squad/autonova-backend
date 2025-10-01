908cd1bab5a86c5487382428fe9092d10b087061
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-10-01 21:07:45 +0530

    initialize backend code base with micro service architecture

---
+++ appointment-booking-service/src/main/java/com/autonova/appointment_booking_service/AppointmentBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.autonova.appointment_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentBookingServiceApplication.class, args);
+	}
+
+}
---
+++ appointment-booking-service/src/test/java/com/autonova/appointment_booking_service/AppointmentBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autonova.appointment_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
