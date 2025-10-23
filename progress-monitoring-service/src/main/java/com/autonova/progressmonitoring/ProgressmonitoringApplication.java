c03d07922acc1d7d699c290bdfaed127e5fac57d
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-10-23 09:08:04 +0530

    initialize progress monitoring service

---
+++ progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/ProgressmonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.autonova.progressmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressmonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressmonitoringApplication.class, args);
+	}
+
+}
