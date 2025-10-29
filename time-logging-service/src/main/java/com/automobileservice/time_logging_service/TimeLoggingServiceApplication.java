59f9feed3c85c049409f5bef2bc13e3f83c5287d
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2025-10-29 18:41:17 +0530

    Initial commit: Set up Spring Boot project structure for Time Logging microservice with PostgreSQL and JPA dependencies.

---
+++ time-logging-service/src/main/java/com/automobileservice/time_logging_service/TimeLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.automobileservice.time_logging_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLoggingServiceApplication.class, args);
+	}
+
+}
