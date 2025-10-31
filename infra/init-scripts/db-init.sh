13f37b1f7f46f3731ffed1437bbd9c94ec8194af
garuka-satharasinghe <methman2002@gmail.com>
2025-11-01 02:39:42 +0530

    feat(notification-service): add initial implementation for notification service with SSE support

--- infra/init-scripts/db-init.sh
+++ infra/init-scripts/db-init.sh
@@ -36,6 +36,7 @@ SERVICES=(
     "projects_db:project_service:PROJECTS_DB_PASSWORD"
     "progress_monitoring_db:progress_monitoring_service:PROGRESS_MONITORING_DB_PASSWORD"
     "employee_dashboard_db:employee_dashboard_service:EMPLOYEE_DASHBOARD_DB_PASSWORD"
+    "notifications_db:notification_service:NOTIFICATION_DB_PASSWORD"
     # Add dbs for serivices as needed
 )
 
