82acecf4bddb0d96870c28a6c17e3025f940d0b6
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-10-31 17:49:35 +0000

    refactor: iimprove Dockerfile structure and update application configuration

--- progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/ProgressmonitoringApplication.java
+++ progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/ProgressmonitoringApplication.java
@@ -2,7 +2,9 @@ package com.autonova.progressmonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
+@EnableRabbit
 @SpringBootApplication
 public class ProgressmonitoringApplication {
 
