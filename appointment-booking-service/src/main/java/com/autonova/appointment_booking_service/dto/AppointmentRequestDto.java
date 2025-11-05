e9d7392e155a2f690e04cade1b3f0054564204e4
tharinedirisinghe <tharinedirisinghe10@gmail.com>
2025-11-05 08:08:26 +0530

    mvn plugin issues resolved

--- appointment-booking-service/src/main/java/com/autonova/appointment_booking_service/dto/AppointmentRequestDto.java
+++ appointment-booking-service/src/main/java/com/autonova/appointment_booking_service/dto/AppointmentRequestDto.java
@@ -1,4 +1,26 @@
 package com.autonova.appointment_booking_service.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+        import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppointmentRequestDto {
+    @NotNull
+    private UUID customerId;
+    @NotNull
+    private UUID vehicleId;
+    @NotNull
+    private String serviceType;
+    @NotNull
+    private OffsetDateTime startTime;
+    @NotNull
+    private OffsetDateTime endTime;
+    // optional: preferred employee
+    private UUID preferredEmployeeId;
+    private String notes;
 }
