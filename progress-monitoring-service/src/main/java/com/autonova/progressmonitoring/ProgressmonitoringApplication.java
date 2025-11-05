6cc683886c3796ba9614dc7395494949f1435b19
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-11-05 22:44:27 +0530

    WIP: eureka service and progress monitoring

--- progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/ProgressmonitoringApplication.java
+++ progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/ProgressmonitoringApplication.java
@@ -3,12 +3,15 @@ package com.autonova.progressmonitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableRabbit
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProgressmonitoringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+    {
 		SpringApplication.run(ProgressmonitoringApplication.class, args);
 	}
 
