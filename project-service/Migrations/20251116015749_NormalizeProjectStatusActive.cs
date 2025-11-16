5844161159d56deb7bd77578fbff0e953913211f
VishwaJaya01 <vishwajayasankha@gmail.com>
2025-11-16 09:30:04 +0530

    merge

---
+++ project-service/Migrations/20251116015749_NormalizeProjectStatusActive.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectService.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizeProjectStatusActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE project."Projects"
+                SET "Status" = 'InProgress'
+                WHERE "Status" = 'Active';
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE project."Projects"
+                SET "Status" = 'Active'
+                WHERE "Status" = 'InProgress';
+            """);
+        }
+    }
+}
