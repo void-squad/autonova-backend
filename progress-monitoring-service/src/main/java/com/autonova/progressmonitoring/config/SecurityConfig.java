31472a2b71da28032f76dba4607cdf93088f151a
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-11-16 06:56:05 +0530

    feat(cors): update allowed origins to include new IP address for CORS configuration

--- progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/config/SecurityConfig.java
+++ progress-monitoring-service/src/main/java/com/autonova/progressmonitoring/config/SecurityConfig.java
@@ -42,7 +42,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "http://139.59.219.225"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
